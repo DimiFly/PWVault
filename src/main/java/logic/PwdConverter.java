d9b1cdcfe53fce2392c32ec085594ee64a72d4da
5im16nivanderheide <5im16nivanderheide@gibwintra.ch>
2018-10-31 13:30:50 +0100

    Added fxml files and java classes

---
+++ src/main/java/logic/PwdConverter.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author 5im16nivanderheide
+ */
+public class PwdConverter {
+
+    public PwdConverter() {
+
+    }
+
+    public String hashPwd(String pwd) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            md.update(pwd.getBytes());
+            byte[] digest = md.digest();
+
+            printHex(digest);
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+}
